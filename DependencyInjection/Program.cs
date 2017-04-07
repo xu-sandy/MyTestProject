91c9009567b761534044e89fd1a098b6f67762d9
baolong <baolong6@qq.com>
2017-04-07 11:22:19 +0800

    ''

---
+++ DependencyInjection/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Samurai;
+using WeaponPro;
+
+namespace DependencyInjection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----Write Target1----");
+            var target1 = Console.ReadLine();
+            Soldier soldier = new Soldier(new Weapon());
+            Console.WriteLine("----" + target1 + ":was hited " + soldier.Attack(target1) + "----");
+            Console.WriteLine("----Write Target2----");
+            var target2 = Console.ReadLine();
+            Soldier soldier2 = new Soldier(new Weapon1());
+            Console.WriteLine("----" + target2 + ":was hited " + soldier2.Attack(target2) + "----");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Samurai/Soldier.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IWeaponPro;
+using WeaponPro;
+
+namespace Samurai
+{
+    public class Soldier
+    {
+        private readonly IWeapon weapon;
+        public Soldier(IWeapon iweapon)
+        {
+            this.weapon = iweapon;
+        }
+        public int Attack(string target)
+        {
+            return this.weapon.Hit(target);
+        }
+    }
+}
